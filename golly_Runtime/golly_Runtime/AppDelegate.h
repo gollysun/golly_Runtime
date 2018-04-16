e3bcaa1efd7839704776c35350e2bd01a42d795e
gollySun <15621101300@163.com>
2018-04-16 20:34:00 +0800

    创建RuntimeDemo

---
+++ golly_Runtime/golly_Runtime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  golly_Runtime
+//
+//  Created by 孙国立 on 2018/4/16.
+//  Copyright © 2018年 孙国立. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ golly_Runtime/golly_RuntimeTests/golly_RuntimeTests.m
@@ -0,0 +1,39 @@
+//
+//  golly_RuntimeTests.m
+//  golly_RuntimeTests
+//
+//  Created by 孙国立 on 2018/4/16.
+//  Copyright © 2018年 孙国立. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface golly_RuntimeTests : XCTestCase
+
+@end
+
+@implementation golly_RuntimeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
